f84e320cf9dd5debf3a1a31c1515abc37c65f2b8
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-06-07 22:17:23 +0500

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Greeter {    
-    string private greeting;   
+    string private greeting;      
 
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
