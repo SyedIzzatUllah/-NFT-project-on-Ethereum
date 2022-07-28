d5e515e2027404c12ad9f77dee87ddc3b2e5bc99
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-07-28 22:21:18 +0500

    Update deploy.js
    token update.

--- deploy.js
+++ deploy.js
@@ -7,7 +7,7 @@ async function main () {
 A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
 so nftContract here is a factory for instances of our GameItem contract.
 */
-  const nftContract = await ethers.getContractFactory("GameItem");
+  const nftContract = await ethers.getContractFactory("GameItem"); 
 
   // here we deploy the contract   
   const deployedNFTContract = await nftContract.deploy();
