84082749602e371290e2476ca56f4bdc62163640
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-07-31 19:52:25 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
-module.exports = { 
+module.exports = {  
   solidity: "0.8.4",
   networks: {
     rinkeby: {
