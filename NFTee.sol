3ffb131fe1fb3f8481e23e08f5488376b336b91a
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-10-21 21:51:57 +0500

    Update NFTee.sol

--- NFTee.sol
+++ NFTee.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // GameItem is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
-contract GameItem is ERC721 {
+contract GameItem is ERC721 { 
 
     constructor() ERC721("GameItem", "ITM") {
         // mint an NFT to yourself
